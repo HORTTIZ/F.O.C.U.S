ae046669952d4cf7122923fbed86e4678ee646df
Yofre Ormaza <97921335+HORTTIZ@users.noreply.github.com>
2022-12-04 12:26:49 -0500

    Actualizar fomulario de envío de emails

---
+++ paginas/email.php
@@ -0,0 +1,40 @@
+<?php 
+
+$correo = $_POST['correo'];
+$nombre = $_POST['nombre'];
+$mensaje = $_POST['mensaje'];
+
+
+//echo $correo . " " . $nombre . " " . $mensaje;
+
+
+$destinatario = "ormaza.main@gmail.com";
+$asunto = "Correo proveniente de FOCUS by HORTTIZ"; 
+$cuerpo = '
+    <html> 
+        <head> 
+            <title>
+                '.$nombre.' Te ha enviado el siguiente mensaje:
+            </title> 
+        </head>
+        <body>
+            <p> 
+                Mensaje: '.$mensaje.'
+                Asunto específico: '.$asunto.'
+            </p> 
+        </body>
+    </html>
+';
+//para el envío en formato HTML 
+$headers = "MIME-Version: 1.0\r\n"; 
+$headers .= "Content-type: text/html; charset=UTF8\r\n"; 
+
+//dirección del remitente
+
+$headers .= "FROM: $nombre <$correo>\r\n";
+mail($destinatario,$asunto,$cuerpo,$headers);
+
+echo "Correo enviado"; 
+?> 
+
+<a href="index.html">Volver a inicio</a>
